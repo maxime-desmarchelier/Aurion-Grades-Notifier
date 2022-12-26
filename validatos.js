52b33a37ba179a0e00912c42dfcfef1c6c1b2bc7
Desmarchelier Maxime <maxime.desmarchelier@ensiie.fr>
2022-12-26 22:46:06 +0100

    Adding config file checker

--- validatos.js
+++ validatos.js
@@ -5,6 +5,9 @@ const config = require("./config");
 let aurion = new aurionHandler();
 let disc = new discordBot();
 
+// Checking config file
+checkConfig();
+
 // Discord bot login
 disc.login().then(() => {
     checkNewGrades();
@@ -15,7 +18,6 @@ let currentSubjects = [];
 
 // Check for new grades every hour
 function checkNewGrades() {
-
     // Get the new subjects list
     aurion.getSubjectList().then((newSubjects) => {
         // Filter the new subjects list to get only the new subjects
@@ -34,3 +36,37 @@ function checkNewGrades() {
         setTimeout(checkNewGrades, config.extra.checkInterval * 60 * 1000);
     });
 }
+
+function checkConfig() {
+    if (!config.extra) {
+        throw "Missing extra section in config file";
+    }
+    if (!config.discord) {
+        throw "Missing discord section in config file";
+    }
+    if (!config.credentials) {
+        throw "Missing credentials section in config file";
+    }
+    if (!config.discord.token) {
+        throw new Error("Missing discord token");
+    }
+    if (!config.discord.logChannelId) {
+        throw new Error("Missing discord log channel id");
+    }
+    if (!config.discord.announcementChannelId) {
+        throw new Error("Missing discord announcement channel id");
+    }
+    if (!config.credentials.user) {
+        throw new Error("Missing Aurion username");
+    }
+    if (!config.credentials.password) {
+        throw new Error("Missing Aurion password");
+    }
+    if (!config.extra || !config.extra.checkInterval) {
+        console.log("Warning : Missing check interval - Using default value : 60 minutes");
+        config['extra'] = {"checkInterval": 60};
+    }
+}
+
+
+
